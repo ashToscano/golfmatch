e9ce45c185a1c7fbed929d5894d10424f6a771b1
Leedabord <gwforumlakers@gmail.com>
2016-10-26 12:49:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -14,20 +14,28 @@ routeJ.run( function ($rootScope, $localStorage, $http) {
         "hcp": [9,15,11,17,13,5,3,7,1,18,12,14,6,8,4,10,2,16]
   }];
   //  https://gwfl.github.io/gsc/courses.json
-  $http.get('courses.json').success(function (jsonData) {
+  $http.get('https://gwfl.github.io/gsc/courses.json').success(function (jsonData) {
    $rootScope.vCourses = angular.copy(jsonData);
   });
   $rootScope.vm00 = { when: " ", loc: " ", pp: 0, ww: 0, tt: 0, 
     cp: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], ch: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     jp: 0, pz4: [30, 15, 8, 5, 2, 0, -2, -4, -6, -8], mip: false };
   $rootScope.vp00 = { nm: " ", id: "", tm: "", th: 0, ts: 0, tw: 0, wolfPts: 0,
-    wolf: [ { role: "", pts: 0, winner: false } ], 
+    wolf: [ { role: "", pts: 0, winner: false }, { role: "", pts: 0, winner: false },
+            { role: "", pts: 0, winner: false }, { role: "", pts: 0, winner: false },
+            { role: "", pts: 0, winner: false }, { role: "", pts: 0, winner: false },
+            { role: "", pts: 0, winner: false }, { role: "", pts: 0, winner: false },
+            { role: "", pts: 0, winner: false }, { role: "", pts: 0, winner: false },
+            { role: "", pts: 0, winner: false }, { role: "", pts: 0, winner: false },
+            { role: "", pts: 0, winner: false }, { role: "", pts: 0, winner: false },
+            { role: "", pts: 0, winner: false }, { role: "", pts: 0, winner: false },
+            { role: "", pts: 0, winner: false }, { role: "", pts: 0, winner: false } ], 
     s: [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
     h: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     w: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     u1: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     u2: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]  };
-  $rootScope.ww00 = [ { role: "hh", hunted: false, } ];   // hh=3; ww=2; lw=4; bw=3x; pg=2x
+  $rootScope.wolf00 = { role: "HH", pts: 0, winner: false, };   // hh=3; ww=2; lw=4; bw=3x; pg=2x
   $rootScope.xxTimes = function (nn) {
    var ii = 0;
    var aa = [];
@@ -42,9 +50,10 @@ routeJ.run( function ($rootScope, $localStorage, $http) {
   };
   $rootScope.clearLSvp = function() {
     delete $localStorage.vp;
-    $localStorage.vp = [];  $localStorage.vp.length = 20;
+    $localStorage.vp = [];  $localStorage.vp.length = 18;
 
-    for (ii = 0; ii < 20; ii++) {
+    for (ii = 0; ii < 18; ii++) {
+      $rootScope.vp00.s[ii] = null;
       $localStorage.vp[ii] = angular.copy($rootScope.vp00);
     }
   };
@@ -111,13 +120,45 @@ routeJ.controller('mainController', function ($scope, $http, $localStorage) {
       $scope.$l_s.vp[jj].h = $scope.uTH($scope.$l_s.vp[jj].th, $scope.$l_s.vm.ch);
     }
   };
-
+  $scope.wolfPts = function(jj) {
+    var wxx = 1, ii = 0;
+    for (ii = 0; ii < $localStorage.vm.pp; ii++) {
+      $localStorage.vp[ii].wolf[jj].pts = 0;
+      if ($localStorage.vp[ii].wolf[jj].winner) {
+        switch ($localStorage.vp[ii].wolf[jj].role) {
+         case 'HH': 
+           $localStorage.vp[ii].wolf[jj].pts = 3;
+           break;
+         case 'LL': 
+           $localStorage.vp[ii].wolf[jj].pts = 4;
+           break;
+         default: 
+           $localStorage.vp[ii].wolf[jj].pts = 2;
+           break;
+        }
+      }
+      switch ($localStorage.vp[ii].wolf[jj].role) {
+       case 'BB': 
+        wxx = 3;
+        break;
+       case 'PP': 
+        wxx = 2;
+        break;
+       default: 
+        break;
+      }
+    }
+    for (ii = 0; ii < $localStorage.vm.pp; ii++) {
+      $localStorage.vp[ii].wolf[jj].pts *= wxx;
+    }
+  };
   $scope.calcTsw = function () {  
     var pz4 = 0; var s = 0; var wxx = 1;
     $localStorage.vm.jp = $localStorage.vm.pp * $localStorage.vm.ww;
     for (ii = 0; ii < $localStorage.vm.pp; ii++) {
       $localStorage.vp[ii].ts = 0;
       $localStorage.vp[ii].tw = 0;
+      $localStorage.vp[ii].wolfPts = 0;
       for (jj = 0; jj < 18; jj++) {
        if ($localStorage.vp[ii].s[jj] !== null) {
         s = $localStorage.vp[ii].s[jj] - $localStorage.vp[ii].h[jj];
@@ -126,35 +167,12 @@ routeJ.controller('mainController', function ($scope, $http, $localStorage) {
         $localStorage.vp[ii].tw += pz4 + $localStorage.vp[ii].u2[jj];
         $localStorage.vp[ii].ts += $localStorage.vp[ii].s[jj] + $localStorage.vm.cp[jj];
 
-        if ($localStorage.vp[ii].wolf[jj].winner) {
-          wxx = 1;
-          switch ($localStorage.vp[ii].wolf[jj].role) {
-            case 'HH': 
-              $localStorage.vp[ii].wolf[jj].pts = 3;
-              break;
-            case 'WW': 
-              $localStorage.vp[ii].wolf[jj].pts = 2;
-              break;
-            case 'LL': 
-              $localStorage.vp[ii].wolf[jj].pts = 4;
-              break;
-            case 'BB': 
-              $localStorage.vp[ii].wolf[jj].pts = 2;
-              wxx = 3;
-              break;
-            case 'PP': 
-              $localStorage.vp[ii].wolf[jj].pts = 2;
-              wxx = 2;
-              break;
-            default: 
-              break;
-          }
-        $localStorage.vp[ii].wolf[jj].pts *= wxx;
-        $localStorage.vp[ii].wolfPts += $localStorage.vp[ii].wolf[jj].pts; 
-        }
-       } }
+        $scope.wolfPts(jj);
+        $localStorage.vp[ii].wolfPts += $localStorage.vp[ii].wolf[jj].pts;
+       }
+      }
       $localStorage.vm.jp -= $localStorage.vp[ii].tw;
-    }  // .\ nested for loops
+    }
     $localStorage.vm.mip = true;
   };
   $scope.adjVP = function (kk, ppIdx, hhIdx, ss) {  
@@ -171,26 +189,7 @@ routeJ.controller('mainController', function ($scope, $http, $localStorage) {
     $scope.calcTsw();
   };
 
-  $scope.wolfPts = function (role, winner, hhIdx, ppIdx) {  
-
-      //  2 wolves win 2 points each
-      //  hunters win 3 points each
-      //  lone wolf wins 4 points
-      //  Blind Wolf: Triple Points for everyone
-      //  Pig:  Double Points for everyone
-
-    switch (role) {
-      case 'ww':    //  ww, hh, lw, bw, pg
-      //  2 wolves win 2 points each
-        break;
-      default:
-        break;
-    }
-    $scope.calcTsw();
-  };
-
   if (angular.isDefined($localStorage.vm) && !$localStorage.vm.mip) { 
-    console.log("vm.pp * .ww:", $localStorage.vm.pp, $localStorage.vm.ww);
     $localStorage.vm.jp = $localStorage.vm.pp * $localStorage.vm.ww;
   }
   $scope.$l_s = $localStorage;
